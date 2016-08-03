68853bc9dc670340b76f62c81876c0d770f178ad
Arkadiy <arkada38@outlook.com>
2016-08-03 12:20:29 +0700

    m

--- Script_Cluster_analysis.R
+++ Script_Cluster_analysis.R
@@ -1,35 +1,32 @@
 # Cluster analysis
 
-beverage.01 <- read.table("Data_beverage.csv", header = T, sep = ";")
-head(beverage.01)
-summary(beverage.01)
+beverage <- read.table("Data_beverage.csv", header = T, sep = ";")
+head(beverage)
+summary(beverage)
 
-beverage.01$numb.obs <- NULL
+beverage$numb.obs <- NULL
 
-# Проводим кластерный анализ,
-# результаты записываем в список clust.beverage
+# We perform cluster analysis,
+# The result is written to the list clust.beverage
 
-clust.beverage <- hclust(dist(beverage.01), "ward.D")
-
-#  Смотрим краткую сводку результатов анализа
+clust.beverage <- hclust(dist(beverage), "ward.D")
 clust.beverage
 # Cluster method   : ward.D 
 # Distance         : euclidean 
 # Number of objects: 34
 
-# Построение дендрограммы
+# Construction of the dendrogram
 plot(clust.beverage)
 rect.hclust(clust.beverage, k = 3, border = "red")
 
-# Разделим пользователей на 3 кластера
-# Вектор groups содержит номер кластера,
-# в который попал классифицируемый объект 
+# Divide into 3 clusters
+# groups vector contains the cluster number 
 groups <- cutree(clust.beverage, k = 3) 
 groups
 
-# в 1-ом кластере
-colMeans(beverage.01[groups==1,]) * 100
-# во 2-ом кластере
-colMeans(beverage.01[groups==2,]) * 100
-# в 3-ем кластере
-colMeans(beverage.01[groups==3,]) * 100
+# in the first cluster
+colMeans(beverage[groups==1,]) * 100
+# in the second cluster
+colMeans(beverage[groups==2,]) * 100
+# in the 3rd cluster
+colMeans(beverage[groups==3,]) * 100
--- Script_Linear_regression.R
+++ Script_Linear_regression.R
@@ -1,26 +1,29 @@
-# Линейная регрессия
+# Linear regression
 # http://www.algorithmist.ru/2011/04/linear-regression-with-examples-in-r.html#more
 
-# Читаем данные из файла
+# Reading data from a file
 data <- read.table('Data_diamond.dat')
 names(data) <- c('Weight', 'Price')
 
 summary(data)
 
-# Строим линейную регрессию
+# Building a linear regression
 fit <- lm(data$Price ~ data$Weight )
-# Печатаем результат
 summary(fit)
+# Coefficients:
+#             Estimate Std. Error t value Pr(>|t|)    
+# (Intercept)  -259.63      17.32  -14.99   <2e-16 ***
+# data$Weight  3721.02      81.79   45.50   <2e-16 ***
 
 # Price = -259.63 + 3721.02 * Weight
-# Модель описывает данные на 97.83%
+# The model describes the data on 97.83%
 
 
 plot(data$Price ~ data$Weight,
      main = "Price ~ Weight",
      sub = "Price = -259.63 + 3721.02 * Weight",
-     xlab = "Вес бриллианта в каратах  (1 карат = 0.2 г.)", 
-     ylab = "Цена кольца в сингапурских долларах")
+     xlab = "The weight of a diamond (carat)", 
+     ylab = "The price of the ring in Singapore dollars")
 abline(fit, col = 2)
 
 
@@ -41,4 +44,4 @@ fit <- lm(data$result ~ data$age + data$shoeSize + data$score )
 summary(fit)
 
 # Result = -0.816931 -0.644451 * age -0.006149 * shoeSize + 0.162203 * score
-# Модель описывает данные на 97.07%
+# The model describes the data on 97.07%
